7329e33ffc5e65e143ddd100f31f443afdb5f8cd
Prashanth-AP <apprashanth11@gmail.com>
2024-12-29 23:20:24 +0530

    data transformation

---
+++ src/components/model_trainer.py
@@ -0,0 +1,141 @@
+# import pandas as pd
+# import numpy as np
+# from sklearn.linear_model import LogisticRegression
+# from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+# from sklearn.tree import DecisionTreeRegressor
+# from sklearn.neighbors import KNeighborsRegressor
+# from src.utils import evaluate_model, save_object
+# from src.exception import CustomException
+# import logging
+# import sys
+# import os,sys
+# from dataclasses import dataclass
+
+# @dataclass
+# class ModelTrainerConfig:
+#     model_obj_file_path=os.path.join('artifacts','bestmodel.pkl')
+
+# class ModelTrainer:
+#     def __init__(self):
+#         self.model_trainer_config=ModelTrainerConfig()
+
+
+# class ModelTrainer:
+#     def initiate_model_training(self,train_array,test_array):
+#         try:
+#             logging.info('Loading train and test data...')
+#             # # Load train and test data
+#             # train_df = pd.read_csv(train_file_path)
+#             # test_df = pd.read_csv(test_file_path)
+
+#             logging.info('Splitting dependent and independent variables...')
+#             # Splitting features (X) and target (y)
+#             X_train = train_array.loc[:, :-1].values  # All rows, all columns except the last
+#             y_train = train_array.loc[:, -1].values   # All rows, last column
+#             X_test = test_array.loc[:, :-1].values
+#             y_test = test_array.loc[:, -1].values
+#             logging.info('Splitting Dependent and Independent variables from train and test data')
+            
+
+#             models = {
+                
+#                 'KNeighborsRegressor': KNeighborsRegressor(),
+#                 'GradientBoostingRegressor': GradientBoostingRegressor(),
+#                 'RandomForestRegressor': RandomForestRegressor(),
+#                 'DecisionTree': DecisionTreeRegressor()
+#             }
+
+#             logging.info('Evaluating models...')
+#             # Evaluate models
+#             model_report: dict = evaluate_model(X_train, y_train, X_test, y_test, models)
+#             print(model_report)
+#             logging.info(f'Model Report: {model_report}')
+
+#             # Identify the best model
+#             best_model_score = max(model_report.values())
+#             best_model_name = [name for name, score in model_report.items() if score == best_model_score][0]
+#             best_model = models[best_model_name]
+
+#             print(f'Best Model Found: {best_model_name} with R2 Score: {best_model_score}')
+#             logging.info(f'Best Model Found: {best_model_name} with R2 Score: {best_model_score}')
+
+#             # Save the best model
+#             # save_object(
+#             #     file_path=self.model_trainer_config.model_obj_file_path,
+#             #     obj=best_model
+#             # )
+            
+            
+          
+
+#         except Exception as e:
+#             logging.error("Exception occurred in the initiate_model_training")
+#             raise CustomException(e, sys)
+
+
+
+
+# src/components/model_trainer.py
+
+import pandas as pd
+import logging
+import sys
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.neighbors import KNeighborsRegressor
+from src.utils import evaluate_model, save_object
+from src.exception import CustomException
+
+
+class ModelTrainer:
+    def __init__(self, model_trainer_config):
+        self.model_trainer_config = model_trainer_config
+
+    def initiate_model_training(self, train_file_path, test_file_path):
+        try:
+            logging.info('Loading train and test data...')
+            # Load train and test data
+            train_df = pd.read_csv(train_file_path)
+            test_df = pd.read_csv(test_file_path)
+
+            logging.info('Splitting dependent and independent variables...')
+            # Splitting features (X) and target (y)
+            X_train = train_df.iloc[:, :-1].values
+            y_train = train_df.iloc[:, -1].values
+            X_test = test_df.iloc[:, :-1].values
+            y_test = test_df.iloc[:, -1].values
+
+            models = {
+                  
+                'KNeighborsRegressor': KNeighborsRegressor(),
+                'GradientBoostingRegressor': GradientBoostingRegressor(),
+                'RandomForestRegressor': RandomForestRegressor(),
+                'DecisionTree': DecisionTreeRegressor()
+            }
+
+            logging.info('Evaluating models...')
+            # Evaluate models
+            model_report: dict = evaluate_model(X_train, y_train, X_test, y_test, models)
+            print(model_report)
+            logging.info(f'Model Report: {model_report}')
+
+            # Identify the best model
+            best_model_score = max(model_report.values())
+            best_model_name = [name for name, score in model_report.items() if score == best_model_score][0]
+            best_model = models[best_model_name]
+
+            print(f'Best Model Found: {best_model_name} with R2 Score: {best_model_score}')
+            logging.info(f'Best Model Found: {best_model_name} with R2 Score: {best_model_score}')
+
+            # Save the best model
+            save_object(
+                file_path=self.model_trainer_config['trained_model_file_path'],
+                obj=best_model
+            )
+            logging.info("best model pkl  file saved")
+
+        except Exception as e:
+            logging.error("Exception occurred in the initiate_model_training")
+            raise CustomException(e, sys)
+
---
+++ src/pipelines/prediction_pipeline.py
@@ -0,0 +1,110 @@
+import sys
+import os
+from src.exception import CustomException
+from src.logger import logging
+from src.utils import load_object
+import pandas as pd
+
+class PredictPipeline:
+    def __init__(self):
+        pass
+
+    def predict(self,features):
+        try:
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            model_path=os.path.join('artifacts','best_model.pkl')
+
+             # Check if the preprocessor file exists
+            if not os.path.exists(preprocessor_path):
+                raise FileNotFoundError("Preprocessor file not found in artifacts directory.")
+
+            # Check if the model file exists
+            if not os.path.exists(model_path):
+                raise FileNotFoundError("Model file not found in artifacts directory.")
+
+            preprocessor=load_object(preprocessor_path)
+            model=load_object(model_path)
+
+            data_scaled=preprocessor.transform(features)
+
+            pred=model.predict(data_scaled)
+            return pred
+            
+
+        except Exception as e:
+            logging.info("Exception occured in prediction")
+            raise CustomException(e,sys)
+        
+
+
+
+class CustomData:
+    def __init__(self,Price:float,
+                 journey_day:int,
+                 journey_month:int,
+                
+                 Airline:str,
+                 Source:str,
+                 Destination:str,
+                 Total_Stops:str):
+                 
+        
+     self.Price=Price
+     self.journey_day=journey_day
+     self.journey_month=journey_month
+   
+     self.Airline=Airline
+     self.Source=Source
+     self.Destination=Destination
+     self.Total_Stops=Total_Stops
+    
+    
+        
+
+    def get_data_as_dataframe(self):
+        try:
+            custom_data_input_dict = {
+                'Price':[self.Price],
+                'journey_day':[self.journey_day],
+                'journey_month':[self.journey_month],
+                
+                'Airline':[self.Airline],
+                'Source':[self.Source],
+                'Destination':[self.Destination],
+                'Total_Stops':[self.Total_Stops]
+            }
+            df = pd.DataFrame(custom_data_input_dict)
+            logging.info('Dataframe Gathered')
+            return df
+        except Exception as e:
+            logging.info('Exception Occured in prediction pipeline')
+            raise CustomException(e,sys)
+   
+
+
+#  Dep_Time_hour:float,
+                #  Dep_Time_minute:float,
+                #  Arrival_Time_hour:float,
+                #  Arrival_Time_minute:float,
+                #  dur_hour:float,
+                #  dur_min:float,
+
+
+                 #  self.Dep_Time_hour=Dep_Time_hour
+    #  self.Dep_Time_minute=Dep_Time_minute
+    #  self.Arrival_Time_hour=Arrival_Time_hour
+    #  self.Arrival_Time_minute=Arrival_Time_minute
+    #  self.dur_hour=dur_hour
+    #  self.dur_min=dur_min
+
+
+    # 'Dep_Time_hour':[self.Dep_Time_hour],
+                # 'Dep_Time_minute':[self.Dep_Time_minute],
+                # 'Arrival_Time_hour':[self.Arrival_Time_hour],
+                # 'Arrival_Time_minute':[self.Arrival_Time_minute],
+                # 'dur_hour':[self.dur_hour],
+                # 'dur_min':[self.dur_min],
+
+
+   
+    
